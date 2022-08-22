3d25fbcd7cf99cdeb3ff316c7b95139a96148611
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2022-08-22 18:54:19 -0500

    Added "self" field & set in awake

--- UnityProjectTemplates/ItemTemplates/MonoBehaviourEditor/NewMonoBehaviourEditor.cs
+++ UnityProjectTemplates/ItemTemplates/MonoBehaviourEditor/NewMonoBehaviourEditor.cs
@@ -7,6 +7,14 @@ namespace $rootnamespace$
     [CustomEditor(nameof($safeitemname$))]
     public class $safeitemname$Editor : Editor
     {
+        $safeitemname$ self;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            self = target as $safeitemname$;
+            return base.CreateInspectorGUI();
+        }
+
         public override void OnInspectorGUI()
         {
             // Draw the GUI unity generates normally
