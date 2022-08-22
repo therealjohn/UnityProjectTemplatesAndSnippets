38b53732475e925577d01d84323948c1287f63c3
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2022-08-22 18:50:49 -0500

    Created first pass MonoBehaviourEditor ItemTemplate

---
+++ UnityProjectTemplates/ItemTemplates/MonoBehaviourEditor/NewMonoBehaviourEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Editor;
+using System.Collections;
+
+namespace $rootnamespace$
+{
+    [CustomEditor(nameof($safeitemname$))]
+    public class $safeitemname$Editor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            // Draw the GUI unity generates normally
+            DrawDefaultInspector();
+        }
+    }
+}
